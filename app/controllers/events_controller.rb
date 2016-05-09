ddf7c9c81aa07e2993df62bd78d2added6313e47
Ardarsh MP <adarshm09@gmail.com>
2016-05-09 23:33:40 +0530

    Add Events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,25 @@
+class EventsController < ApplicationController
+	def index
+		@events = Event.all.order('created_at DESC')
+	end
+
+	 def new
+
+	 end
+
+	 def create
+		@event = Event.new(event_params)
+		@event.save
+		redirect_to @event, notice: "Successfully create new event"
+		
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:title, :details)
+	end
+end
